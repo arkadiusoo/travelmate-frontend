b440518a405ceaa9439d1af706f4239c575bf345
arkadiusoo <arkadiusz.mlyn@icloud.com>
2025-05-03 21:43:46 +0200

    logo added to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import ThemeToggle from "./ThemeToggle";
+import logo from "../assets/logo.png";
 
 function NavigationBar({ onLoginClick }) {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="#">TravelMate</Navbar.Brand>
+        <Navbar.Brand href="#" className="d-flex align-items-center gap-2">
+          <img
+            src={logo}
+            alt="Logo"
+            height="30"
+            style={{ objectFit: "contain" }}
+          />
+          TravelMate
+        </Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse>
           <Nav className="ms-auto d-flex align-items-center gap-2">
